a44bbca749b3b83aee4af698a3b82385f82774a1
Miro7543 <miroslavmihaylov75432@gmail.com>
2021-10-25 18:07:06 +0300

    Changing startup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const io=require('socket.io')(3000,{
     cors:{
-        origin:['http://127.0.0.1:5500']
+        origin:['https://bikove-kravi.herokuapp.com/']
     }
 });
 const { Room } = require('./constructors.js');
